5d2c23ae9b07e1f3e0d26473b3089cb4743ed34a
Dionathan Silva <dionideev@gmail.com>
2024-09-01 21:47:02 -0300

    alternar entre registros dentro do edit

--- app/Filament/Resources/AcertoResource/Pages/EditAcerto.php
+++ app/Filament/Resources/AcertoResource/Pages/EditAcerto.php
@@ -5,15 +5,20 @@ namespace App\Filament\Resources\AcertoResource\Pages;
 use App\Filament\Resources\AcertoResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use JoseEspinal\RecordNavigation\Traits\HasRecordNavigation;
 
 class EditAcerto extends EditRecord
 {
+    use HasRecordNavigation;
+    
     protected static string $resource = AcertoResource::class;
 
     protected function getHeaderActions(): array
     {
-        return [
-            Actions\DeleteAction::make(),
-        ];
+        return array_merge(parent::getHeaderActions(), $this->getNavigationActions());
+
+        // return [
+        //     Actions\DeleteAction::make(),
+        // ];
     }
 }
--- app/Filament/Resources/AcertoResource/Pages/ListAcertos.php
+++ app/Filament/Resources/AcertoResource/Pages/ListAcertos.php
@@ -5,9 +5,12 @@ namespace App\Filament\Resources\AcertoResource\Pages;
 use App\Filament\Resources\AcertoResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use JoseEspinal\RecordNavigation\Traits\HasRecordsList;
 
 class ListAcertos extends ListRecords
 {
+    use HasRecordsList;
+        
     protected static string $resource = AcertoResource::class;
 
     protected function getHeaderActions(): array
