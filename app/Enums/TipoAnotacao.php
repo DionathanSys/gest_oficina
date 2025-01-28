f49dd4bbcc74ba9b4440a07d52ca77be7f7ffdbb
Dionathan Silva <dionideev@gmail.com>
2025-01-28 14:07:38 -0300

    fix: ajuste em opções do Enum TipoAnotação

--- app/Enums/TipoAnotacao.php
+++ app/Enums/TipoAnotacao.php
@@ -8,13 +8,13 @@ enum TipoAnotacao: string
     case ACOMPANHAMENTO         = 'ACOMPANHAMENTO';
 
     case MANUTENCAO             = 'MANUTENÇÃO';
-    case INSPENCAO_PERIODICA    = 'INSPENÇÃO PERIODICA';
-    case INSPENCAO_MANT         = 'INSPENÇÃO MANT';
-    case INSPENCAO_PREVENTIVA   = 'INSPENÇÃO PREVENTIVA';
+    case INSPECAO_PERIODICA     = 'INSPEÇÃO PERIODICA';
+    case INSPECAO_MANT          = 'INSPEÇÃO MANT';
+    case INSPECAO_PREVENTIVA    = 'INSPEÇÃO PREVENTIVA';
     
     case PNEU                   = 'PNEU';
     case MOV_PNEU               = 'MOV PNEU';
-    case INSPENCAO_PNEU         = 'INSPENÇÃO PNEU';
+    case INSPECAO_PNEU          = 'INSPEÇÃO PNEU';
 
     
     // public function getOptions(): string
