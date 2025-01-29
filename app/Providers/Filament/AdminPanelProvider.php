15d1793eef3474968456e2b42f88ff94a12b667f
Dionathan Silva <dionideev@gmail.com>
2025-01-29 15:08:24 -0300

    ajustes para mostrar comentarios

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -24,7 +24,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->unsavedChangesAlerts()
+            // ->unsavedChangesAlerts()
             ->maxContentWidth(MaxWidth::Full)
             ->sidebarCollapsibleOnDesktop()
             ->brandName('SysSoft | AutoMaster')
