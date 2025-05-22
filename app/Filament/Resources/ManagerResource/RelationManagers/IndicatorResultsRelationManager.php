1bca4df1b476dc64e59f2f96a568e554574038a5
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-05-22 09:37:33 -0300

    fix

--- app/Filament/Resources/ManagerResource/RelationManagers/IndicatorResultsRelationManager.php
+++ app/Filament/Resources/ManagerResource/RelationManagers/IndicatorResultsRelationManager.php
@@ -124,8 +124,10 @@ class IndicatorResultsRelationManager extends RelationManager
                     }),
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+                Tables\Actions\EditAction::make()
+                    ->iconButton(),
+                Tables\Actions\DeleteAction::make()
+                    ->iconButton(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
